caa6b6e88caadc0153d0b8fa86cd43b8f8090b2f
Chloe McEntire <chloemcentire@gmail.com>
2020-08-16 10:49:27 -0500

    comment table cannot accept null input

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_033950) do
+ActiveRecord::Schema.define(version: 2020_08_16_154339) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 2020_08_07_033950) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
+    t.string "commenter", null: false
+    t.text "body", null: false
     t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
